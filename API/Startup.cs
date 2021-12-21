2ec216c3622761d1d1f0007a036563cf43a81c28
Peter M <sd45x@users.noreply.github.com>
2021-12-21 13:26:28 -0800

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using System.Runtime.Serialization;
+using System.Net.Security;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,6 +41,13 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +63,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
